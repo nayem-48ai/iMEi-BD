ec2d4bcb11e64bbfa41018c11c62a468b67efe3a
nayem-48ai <150437672+nayem-48ai@users.noreply.github.com>
2026-01-02 10:27:10 +0600

    Create LoadingSpinner.js

---
+++ components/LoadingSpinner.js
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="spinner-border spinner-border-sm text-light" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
+}
