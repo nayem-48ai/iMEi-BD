00bc96ae3615595633411780e4bc4796d90c0b02
nayem-48ai <150437672+nayem-48ai@users.noreply.github.com>
2026-01-02 10:46:45 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 "use client";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import { ThemeProvider } from '@/context/ThemeContext';
@@ -11,22 +11,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>NEIR Advance Portal</title>
+        <title>NEIR Advance Portal | Tnayem48</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
       </head>
       <body>
         <ThemeProvider>
-          <div className="d-flex">
+          <div className="d-flex overflow-hidden">
             <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
-            <div className="main-content w-100" style={{ marginLeft: isOpen ? '250px' : '0', transition: '0.3s' }}>
+            <div className="flex-grow-1 min-vh-100 d-flex flex-column" style={{ 
+              marginLeft: isOpen ? '0px' : '0', 
+              transition: '0.3s' 
+            }}>
               <Navbar toggleSidebar={() => setIsOpen(!isOpen)} />
-              <main className="container mt-4 p-4">
+              <main className="container-fluid p-3 p-md-4">
                 {children}
               </main>
             </div>
           </div>
         </ThemeProvider>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
       </body>
     </html>
   );
