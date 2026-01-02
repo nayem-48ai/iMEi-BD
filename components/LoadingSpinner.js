fa043e138193ebd6732739393ba3419a514b6d8a
nayem-48ai <150437672+nayem-48ai@users.noreply.github.com>
2026-01-02 12:19:03 +0600

    Delete components directory

--- components/LoadingSpinner.js
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="spinner-border spinner-border-sm text-light" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-  );
-}
