8358fb543f70104e54e231ed313f844484c96cd8
nayem-48ai <150437672+nayem-48ai@users.noreply.github.com>
2026-01-02 19:28:08 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,30 @@
+"use client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
 import { ThemeProvider } from '@/context/ThemeContext';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
+import { useEffect } from 'react';
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <html lang="en">
+      <head>
+        <title>NEIR IMEI BD - Advanced</title>
+      </head>
       <body>
         <ThemeProvider>
-          <div className="d-flex">
+          <div className="d-flex overflow-hidden" style={{ minHeight: '100vh' }}>
+            {/* Desktop Sidebar */}
             <Sidebar />
-            <div className="flex-grow-1">
+            
+            <div className="flex-grow-1 d-flex flex-column" style={{ width: '100%', overflowY: 'auto' }}>
               <Navbar />
-              <main className="container mt-4 p-3">
+              <main className="container-fluid p-3 p-md-4">
                 {children}
               </main>
             </div>
